aca75b6e9ac2f6557854d6383beaa5c4b16f1e28
Ubuntu <ubuntu@ip-172-31-41-46.ec2.internal>
2022-04-05 18:41:12 +0000

    Finished Grandma File

--- grandmother.py
+++ grandmother.py
@@ -4,16 +4,17 @@
 # Created on March 2022
 # This is a program that checks to see if you are allowed to date a granddaughter
 
+
 def main():
     # This function is the main program
-    
+
     # Input
     print("Answer Yes/No to see if you can date the granddaughter.")
     rich = input("Are you rich?: ")
     handsome = input("are you handsome?: ")
-    
+
     # Process and Output
-    
+
     if rich == "yes" or handsome == "yes":
         print("You can date the GrandDaughter")
     elif rich == "no" and handsome == "no":
